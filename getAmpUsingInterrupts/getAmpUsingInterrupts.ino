462682679ee2e99bdede3b6a1332534b443cf9a0
deepmistry96 <deepmistry96@users.noreply.github.com>
2018-10-09 21:40:56 -0500

    teensyInturrupt instread of arduino

--- getAmpUsingInterrupts/getAmpUsingInterrupts.ino
+++ getAmpUsingInterrupts/getAmpUsingInterrupts.ino
@@ -2,31 +2,25 @@
 
 //const int mic = A0;
 
+IntervalTimer timerAmp; //we can have upto 4 timers going simultaniously but unlimited timer objects
+
 int mic = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  Timer1.initialize(2000); //triggers 5 times a second
-  Timer1.attachInterrupt(getMicVal);
+  //Timer1.initialize(2000); //triggers 5 times a second
+  //Timer1.attachInterrupt(getMicVal);
   Serial.begin(115200);
+  timerAmp.begin(getMicVal, 150000); //getMicVal() will run every .15 seconds
 }
 
 void loop() {
 
-  ///unsigned long micCopy;
-
-  
-
-  
-  
-  
-  
-  
   
   
 }
 
-void getMicVal(void)
+void getMicVal()
 {
   mic = analogRead(A0);
   Serial.println(mic);
